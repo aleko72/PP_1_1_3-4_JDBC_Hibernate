9ef8daa5dfafdcf0d629f8049aa7c2ab093de02c
Alexandr Kolushkin <kolushkin@gmail.com>
2023-05-11 09:28:04 +0300

    task_1.1.5_v3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -4,6 +4,7 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.util.List;
 
@@ -14,11 +15,15 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        Transaction tr = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            session.beginTransaction();
+            tr = session.beginTransaction();
             session.createSQLQuery(Util.QUERY_CREATE_TABLE).executeUpdate();
-            session.getTransaction().commit();
+            tr.commit();
         }catch (Exception e){
+            if(tr != null){
+                tr.rollback();
+            }
             e.printStackTrace();
             throw e;
         }
@@ -26,11 +31,15 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void dropUsersTable() {
+        Transaction tr = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            session.beginTransaction();
+            tr = session.beginTransaction();
             session.createSQLQuery(Util.QUERY_DROP_TABLE).executeUpdate();
-            session.getTransaction().commit();
+            tr.commit();
         }catch (Exception e){
+            if(tr != null){
+                tr.rollback();
+            }
             e.printStackTrace();
             throw e;
         }
@@ -38,12 +47,15 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        User user = new User(name, lastName, age);
+        Transaction tr = null;
         try(Session session = Util.getSessionFactory().openSession()) {
-            Transaction tr = session.beginTransaction();
-            session.save(user);
+            tr = session.beginTransaction();
+            session.save(new User(name, lastName, age));
             tr.commit();
         }catch (Exception e){
+            if(tr != null){
+                tr.rollback();
+            }
             e.printStackTrace();
             throw e;
         }
@@ -51,12 +63,16 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void removeUserById(long id) {
+        Transaction tr = null;
         try(Session session = Util.getSessionFactory().openSession()) {
-            Transaction tr = session.beginTransaction();
+            tr = session.beginTransaction();
             User user = session.get(User.class, id);
             session.delete(user);
             tr.commit();
         }catch (Exception e){
+            if(tr != null){
+                tr.rollback();
+            }
             e.printStackTrace();
             throw e;
         }
@@ -65,7 +81,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public List<User> getAllUsers() {
         try (Session session = Util.getSessionFactory().openSession()) {
-           return (List<User>) session.createQuery("From User").list();
+          return session.createQuery("from User", User.class).list();
         }catch (Exception e){
             e.printStackTrace();
             throw e;
@@ -74,11 +90,15 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
+        Transaction tr = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction tr = session.beginTransaction();
+            tr = session.beginTransaction();
             session.createQuery("Delete From User").executeUpdate();
             tr.commit();
         }catch (Exception e){
+            if(tr != null){
+                tr.rollback();
+            }
             e.printStackTrace();
             throw e;
         }
